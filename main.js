0f1bbb3f5ede590a668041b4c12c4192c1483cac
Perry Fustero <pjfust@gmail.com>
2017-07-13 19:27:07 -0400

    change key colors, labeled js better

--- main.js
+++ main.js
@@ -1,103 +1,36 @@
-let score = document.getElementById('score')
+// --------------------------- VARIABLES ---------------------------
+let score = document.getElementById('score') // DOCUMENT IDENTIFIERS
 let feedback = document.getElementById('feedback')
-let lightsOffSwitch = document.getElementById('lightsToggle')
-let blackKeysSwitch = document.getElementById('blackKeysToggle')
 let secondCount = document.getElementById('timer')
 
+let userArray = [] // MISC VARIABLES
+let compArray = []
 let lightsOff = false
 let blackKeysOn = false
 let totalKeys = 8
-let time = 800
-let timeSubtracter = 20
-let startKeyTimer = true
-let keyTimer = 0
-let keyTimerArr = []
 let listen = true
 let scoreCount = false
-let countDownTimer
 
-let userArray = []
-let compArray = []
+let time = 800 // TIME BETWEEN NOTES
+let timeSubtracter = 30
 
+let countDownTimer // COUNT DOWN UNTIL GAME OVER
 let timerReady = true
 let timer = 10
 let timerAdder = 0
 
-// ---------------------------BUTTONS ---------------------------
-
-function startTimer () {
-  timer = 10
-  timerAdder++
-  timer += timerAdder
-  if (timerReady && timer) {
-    timerReady = false
-    countDownTimer = setInterval(() => {
-      timer--
-      if (timer > 0) {
-        secondCount.innerHTML = timer
-      } else if (timer <= 0) {
-        timer = 0
-        secondCount.innerHTML = timer
-        gameOver()
-      }
-    }, 1000)
-  }
-}
-
-function stopTimer () {
-  clearInterval(countDownTimer)
-  timerReady = true
-}
-
-var buttonNormal = document.getElementById('buttonNormal')
-buttonNormal.addEventListener('click', function () {
-  compArray = []
-  userArray = []
-  scoreCount = true
-  timerAdder = 0
-  score.innerHTML = '0'
-  feedback.innerHTML = ''
-  compBox.randomize()
-})
-
-var buttonFreePlay = document.getElementById('buttonFreePlay')
-buttonFreePlay.addEventListener('click', function () {
-  feedback.innerHTML = 'Free Play'
-  scoreCount = false
-  listen = true
-  userArray = []
-})
-
-lightsOffSwitch.addEventListener('click', function () {
-  if (lightsOffSwitch.checked) {
-    lightsOff = true
-  } else if (!lightsOffSwitch.checked) {
-    lightsOff = false
-  }
-})
-
-blackKeysSwitch.addEventListener('click', function () {
-  if (blackKeysSwitch.checked) {
-    blackKeysOn = true
-    totalKeys = 13
-  } else if (!blackKeysSwitch.checked) {
-    blackKeysOn = false
-    totalKeys = 8
-  }
-})
-
-let buttonReferenceC = document.getElementById('buttonReferenceC')
-buttonReferenceC.addEventListener('click', function () {
-  keys[0].playSound()
-})
+let startKeyTimer = true // WORKAROUND TO AVOID BUTTON SMASHES
+let keyTimer = 0
+let keyTimerArr = []
 
 // --------------------------- KEY CLASS ---------------------------
 
 class Key {
-  constructor (el, sound, num) {
+  constructor (el, sound, num, color) {
     this.el = document.getElementById(el)
     this.sound = document.getElementById(sound)
     this.num = num
+    this.color = color
     this.el.addEventListener('click', () => {
       if (listen) {
         this.playSound()
@@ -108,7 +41,19 @@ class Key {
   playSound () {
     this.sound.play()
     if (!lightsOff) {
-      this.el.classList.add('keyPress')
+      switch (this.color) {
+        case 'blue':
+          this.el.classList.add('keyPressB')
+          break
+        case 'yellow':
+          this.el.classList.add('keyPressY')
+          break
+        case 'green':
+          this.el.classList.add('keyPressG')
+          break
+        case 'red':
+          this.el.classList.add('keyPressR')
+      }
     }
     if (listen) { // Prevents User from Bashing Single Key
       if (startKeyTimer === true) {
@@ -129,7 +74,19 @@ class Key {
       }, 500)
     }
     setTimeout(() => {
-      this.el.classList.remove('keyPress')
+      switch (this.color) {
+        case 'blue':
+          this.el.classList.remove('keyPressB')
+          break
+        case 'yellow':
+          this.el.classList.remove('keyPressY')
+          break
+        case 'green':
+          this.el.classList.remove('keyPressG')
+          break
+        case 'red':
+          this.el.classList.remove('keyPressR')
+      }
     }, 500)
   }
   arrayPush () {
@@ -154,12 +111,15 @@ class Key {
   }
 }
 
+// --------------------------- GAME OVER / CORRECT ---------------------------
+
 function gameOver () {
   score.innerHTML = compArray.length - 1
   feedback.innerHTML = 'Game over'
   stopTimer()
   listen = false
   time = 800
+  // for (let i = )
 }
 
 function correct () {
@@ -205,17 +165,89 @@ var compBox = {
 // --------------------------- KEYS ARRAY ---------------------------
 
 let keys = [
-  new Key('keyLowC', 'lowCNote', 0),
-  new Key('keyD', 'dnote', 1),
-  new Key('keyE', 'enote', 2),
-  new Key('keyF', 'fnote', 3),
-  new Key('keyG', 'gnote', 4),
-  new Key('keyA', 'anote', 5),
-  new Key('keyB', 'bnote', 6),
-  new Key('keyHighC', 'highCNote', 7),
-  new Key('keyCS', 'csnote', 8),
-  new Key('keyDS', 'dsnote', 9),
-  new Key('keyFS', 'fsnote', 10),
-  new Key('keyGS', 'gsnote', 11),
-  new Key('keyAS', 'asnote', 12)
+  new Key('keyLowC', 'lowCNote', 0, 'blue'),
+  new Key('keyD', 'dnote', 1, 'green'),
+  new Key('keyE', 'enote', 2, 'blue'),
+  new Key('keyF', 'fnote', 3, 'yellow'),
+  new Key('keyG', 'gnote', 4, 'red'),
+  new Key('keyA', 'anote', 5, 'yellow'),
+  new Key('keyB', 'bnote', 6, 'red'),
+  new Key('keyHighC', 'highCNote', 7, 'blue'),
+  new Key('keyCS', 'csnote', 8, 'yellow'),
+  new Key('keyDS', 'dsnote', 9, 'red'),
+  new Key('keyFS', 'fsnote', 10, 'green'),
+  new Key('keyGS', 'gsnote', 11, 'blue'),
+  new Key('keyAS', 'asnote', 12, 'green')
 ]
+
+// ---------------------------BUTTONS ---------------------------
+
+var buttonStart = document.getElementById('buttonStart')
+buttonStart.addEventListener('click', function () {
+  compArray = []
+  userArray = []
+  scoreCount = true
+  timerAdder = 0
+  score.innerHTML = '0'
+  feedback.innerHTML = ''
+  compBox.randomize()
+})
+
+var buttonFreePlay = document.getElementById('buttonFreePlay')
+buttonFreePlay.addEventListener('click', function () {
+  feedback.innerHTML = 'Free Play'
+  scoreCount = false
+  listen = true
+  userArray = []
+})
+
+let lightsOffSwitch = document.getElementById('lightsToggle')
+lightsOffSwitch.addEventListener('click', function () {
+  if (lightsOffSwitch.checked) {
+    lightsOff = true
+  } else if (!lightsOffSwitch.checked) {
+    lightsOff = false
+  }
+})
+
+let blackKeysSwitch = document.getElementById('blackKeysToggle')
+blackKeysSwitch.addEventListener('click', function () {
+  if (blackKeysSwitch.checked) {
+    blackKeysOn = true
+    totalKeys = 13
+  } else if (!blackKeysSwitch.checked) {
+    blackKeysOn = false
+    totalKeys = 8
+  }
+})
+
+let buttonReferenceC = document.getElementById('buttonReferenceC')
+buttonReferenceC.addEventListener('click', function () {
+  keys[0].playSound()
+})
+
+// ---------------------------COUNTDOWN TIMER---------------------------
+
+function startTimer () {
+  timer = 10
+  timerAdder++
+  timer += timerAdder
+  if (timerReady && timer) {
+    timerReady = false
+    countDownTimer = setInterval(() => {
+      timer--
+      if (timer > 0) {
+        secondCount.innerHTML = timer
+      } else if (timer <= 0) {
+        timer = 0
+        secondCount.innerHTML = timer
+        gameOver()
+      }
+    }, 1000)
+  }
+}
+
+function stopTimer () {
+  clearInterval(countDownTimer)
+  timerReady = true
+}
